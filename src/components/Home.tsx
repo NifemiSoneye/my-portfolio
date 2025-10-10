a875012400a7e8b500c84c1cbebd16a150b78188
Nifemi Soneye <nifemisoneye@gmail.com>
2025-10-09 22:29:26 -0700

    Added dev icon and quote message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,8 +3,9 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   const name: string[] = "Nifemi".split("");
+  const devIcon = new URL("../assets/developer.png", import.meta.url).href;
   return (
-    <div className="pt-[6rem] text-white text-center">
+    <section id="home" className="pt-[6rem] text-white text-center">
       <h1 className="text-[48px]">Hey There ,</h1>
       <motion.h1
         className="text-[48px]"
@@ -17,8 +18,8 @@ const Home = () => {
           <motion.span
             key={index}
             className="text-green-400 inline-block"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, x: 0 }}
             transition={{
               delay: index * 0.1, // delay per letter
               duration: 0.3,
@@ -38,7 +39,17 @@ const Home = () => {
         </motion.span> */}
       </motion.h1>
       <p className="text-[24px] pt-[2rem]">A Frontend Developer</p>
-    </div>
+      <div className="flex items-center justify-center mt-[2rem] ">
+        <div className="h-[300px] w-[300px] rounded-full bg-[#212A37] border-2 border-[#222428] flex items-center justify-center p-[1rem]">
+          <img
+            src={devIcon}
+            alt="dev"
+            className="h-[220px] w-[220px] p-[1rem] "
+          />
+        </div>
+      </div>
+      <p className="italic mt-[1rem]">"Build for everyone with everyone"</p>
+    </section>
   );
 };
 
