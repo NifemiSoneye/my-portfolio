a04841eee2bbd8abaecea9cd7f8a50a7b9cb2a44
Nifemi Soneye <nifemisoneye@gmail.com>
2025-10-09 23:45:17 -0700

    Made the page responsive for larger screens

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,9 +6,9 @@ const Nav = () => {
     .href;
   return (
     <div className="backdrop-blur-md fixed top-0 left-0 w-full mx-[0.5rem] border-b border-gray-600">
-      <div className="flex justify-between mt-[1rem] text-white pb-[1rem]">
+      <div className="flex justify-between mt-[1rem] text-white pb-[1rem] lg:mt-[2rem] items-center lg:mx-[1.5rem]">
         <h1 className="text-[1.5rem]">NIFEMI</h1>
-        <ul className="hidden lg:flex mx-[1rem]">
+        <ul className="hidden lg:flex mx-[2rem] text-[22px] ">
           {navLinks.map((link) => (
             <li key={link} className="capitalize cursor-pointer">
               <Link
@@ -16,7 +16,7 @@ const Nav = () => {
                 smooth={true}
                 duration={700}
                 offset={-80}
-                className="hover:text-green-400 ml-[0.5rem] text-white"
+                className="hover:text-green-400 ml-[1rem] text-white"
               >
                 {link}
               </Link>
