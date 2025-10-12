db146ad0f7c86b569f1876d8853e321d3d1bafc4
Nifemi Soneye <nifemisoneye@gmail.com>
2025-10-12 10:17:27 -0700

    Initialized skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Nav />
         <Home />
         <About />
+        <Skills />
       </div>
     </>
   );
