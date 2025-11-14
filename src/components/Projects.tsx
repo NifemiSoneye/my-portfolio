d7636952e983518fc1d86304980f54840ab44907
Your GitHub Username <nifemisoneye@gmail.com>
2025-11-14 02:05:00 -0800

    Finished project mobile-view

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,39 +1,149 @@
-import React from 'react'
+import React from "react";
 import { motion, useInView, type Variants } from "framer-motion";
 import { useRef } from "react";
 
 const Projects = () => {
-    const variants: Variants = {
-        hidden: { opacity: 0, x: 50 }, // start off invisible & below
-        visible: {
-          opacity: 1,
-          x: 0,
-          transition: { duration: 0.8, ease: "easeOut" },
-        },
-      };
-      const ref = useRef(null);
-        const isInView = useInView(ref, {
-          once: false,
-          amount: 0.2,
-        });
+  const variants: Variants = {
+    hidden: { opacity: 0, y: 50 }, // start off invisible & below
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+  const projectVariants: Variants = {
+    hidden: { opacity: 0, x: 50 }, // start off invisible & below
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    once: false,
+    amount: 0.2,
+  });
+  const projects = [
+    {
+      title: "IP-TRACKER",
+      desc: " A utility web app that allows users to track IP addresses and see geolocation data on a map in real time. Built with React, Tailwind CSS, LeafletJS, and the IPify API, it demonstrates API integration, state management, and responsive UI design for practical tools.",
+      mobileImg: new URL("../assets/IP-tracker-mobile.png", import.meta.url)
+        .href,
+      desktopImg: new URL("../assets/IP-tracker-desktop.png", import.meta.url)
+        .href,
+      demo: "https://nifemisoneye.github.io/IP_tracker/",
+      source: "https://github.com/NifemiSoneye/IP_tracker",
+    },
+    {
+      title: "MULTI-FORM ",
+      desc: "A dynamic, responsive form that guides users through multiple steps with validation at each stage. Implemented using React, Tailwind CSS and Typescript it emphasizes form state handling, conditional rendering, and user-friendly feedback, showcasing polished frontend interactions as well as form validation and error handling.",
+      mobileImg: new URL("../assets/multi-form-mobile.png", import.meta.url)
+        .href,
+      desktopImg: new URL("../assets/multi-form-desktop.png", import.meta.url)
+        .href,
+      demo: "https://multi-step-form-kohl-kappa.vercel.app/",
+      source: "https://github.com/NifemiSoneye/Multi-step-form",
+    },
+    {
+      title: "SPACE-TOURISM WEBSITE",
+      desc: "A multi-page, responsive web project inspired by a space travel theme. Built with React and Tailwind CSS, it features smooth navigation, dynamic content, and immersive animations. Users can explore destinations, learn about crew members, and experience a visually rich, interactive interface.",
+      mobileImg: new URL("../assets/space-tourism-mobile.png", import.meta.url)
+        .href,
+      desktopImg: new URL(
+        "../assets/space-tourism-desktop.png",
+        import.meta.url
+      ).href,
+      demo: "https://spacetourismsolution.netlify.app/",
+      source: "https://github.com/NifemiSoneye/Space_tourism",
+    },
+    {
+      title: "COUNTRY-API WEBSITE",
+      desc: "A responsive web app that displays data about countries using the REST Countries API. Built with React , Typescript and Tailwind CSS, it allows users to search, filter, and view detailed country information. The project highlights API integration, dynamic rendering, and a clean, user-friendly interface.",
+      mobileImg: new URL("../assets/rest-countries-mobile.png", import.meta.url)
+        .href,
+      desktopImg: new URL(
+        "../assets/rest-countries-desktop.png",
+        import.meta.url
+      ).href,
+      demo: "https://rest-countries-app-wine.vercel.app/",
+      source: "http://github.com/NifemiSoneye/rest-countries-app",
+    },
+  ];
   return (
     <section
-      id="skills"
+      id="projects"
       className="min-h-screen bg-[#111113] text-white overflow-x-hidden border-b border-gray-500 pb-[1rem] "
     >
-        <div className="w-full flex justify-center">
-            <motion.div
-        ref={ref}
-        variants={variants}
-        initial="hidden"
-        animate={isInView ? "visible" : "hidden"} // 👈 animate when in view
-        className="flex flex-col justify-center max-w-3xl mx-auto px-6"
-      >
-        
-      </motion.div>
+      <div className="w-full flex justify-center flex-col">
+        <motion.div
+          ref={ref}
+          variants={variants}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"} // 👈 animate when in view
+          className="flex flex-col justify-center max-w-3xl mx-auto px-6"
+        >
+          <h2 className="text-[48px] opacity-50 text-green-400 font-bold my-[4rem] w-full lg:ml-[-10rem] lg:text-[8rem]">
+            My Projects
+          </h2>
+        </motion.div>
+        <div className="flex flex-col">
+          {projects.map((project, index) => {
+            const projectRef = useRef(null);
+            const projectInView = useInView(projectRef, {
+              once: false,
+              amount: 0.4,
+            });
+            return (
+              <motion.div
+                key={index}
+                ref={projectRef}
+                initial={{ opacity: 0, y: 50 }}
+                animate={
+                  projectInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }
+                }
+                transition={{ duration: 0.7, type: "spring" }}
+                className="flex-col lg:flex-row items-center justify-center gap-6 my-12 px-4 max-w-6xl mx-auto min-h-[40vh]"
+              >
+                <div className="flex items-center justify-start flex-col-reverse">
+                  <div>
+                    <h3 className="text-xl font-bold">{project.title}</h3>
+                    <p className="text-[22px]">{project.desc}</p>
+                    <div className="flex mt-[1rem] ml-[1rem]">
+                      <a
+                        href={project.demo}
+                        className="border border-white p-[1rem] mr-[1rem] hover:bg-green-400"
+                      >
+                        View Demo
+                      </a>
+                      <a
+                        href={project.source}
+                        className="border border-white p-[1rem] hover:bg-green-400"
+                      >
+                        Source Code
+                      </a>
+                    </div>
+                  </div>
+                  <div className="flex items-start relative">
+                    <img
+                      src={project.desktopImg}
+                      alt={`${project.title} mobile`}
+                      className="w-[500px] h-[350px]"
+                    />
+                    <img
+                      src={project.mobileImg}
+                      alt={`${project.title} desktop`}
+                      className="w-[100px] h-[202px] absolute left-[70%] bottom-[10%]"
+                    />
+                  </div>
+                </div>
+              </motion.div>
+            );
+          })}
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
